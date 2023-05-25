5e049ec3ee70a163ca4a37d5c9b7af3892b20759
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-05-24 20:27:41 -0400

    adding middleware

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require('http');
 
-const server = http.createServer();
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('In the middleware - express');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('In the another middleware - express');
+    res.send('<h1>This is my first application.</h1>');
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
