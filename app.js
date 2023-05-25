94d8f7c17c62b43d0bb31feec49d41de2565f35a
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-05-24 21:07:58 -0400

    Handle different routes

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
-const http = require('http');
-
 const express = require('express');
-
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware - express');
-    next();
+app.use('/add-product',(req, res, next) => {
+    console.log('add-product');
+    res.send('<h1>The add product page.</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/',(req, res, next) => {
     console.log('In the another middleware - express');
     res.send('<h1>This is my first application.</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
